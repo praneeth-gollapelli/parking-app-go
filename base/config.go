547af8e19e14e25d695cd2c7280a6824da2dae97
Praneeth Gollapelli <praneeth@touchnote.in>
2021-03-09 17:33:45 +0530

    Test1 commit

--- base/config.go
+++ base/config.go
@@ -1,6 +1,7 @@
 package base
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"parking-app-go/model"
@@ -30,5 +31,6 @@ func LoadConfig(config model.Config) model.Config {
 			log.Fatalf("Invalid .env file errors: %s", err)
 		}
 	}
+	fmt.Println("test commit1")
 	return config
 }
